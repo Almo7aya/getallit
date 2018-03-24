f5fd4c7c46c39f8a8076bfafdc1b8e57514cc2be
Ali Almohaya <alialmohaya@gmail.com>
2018-03-24 21:04:29 +0300

    add the init of the app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// the main file for the express app
+
+const express = require('express');
+const app = express();
+const CORS = require('cors');
+
+// setting the CORS middle-ware
+app.use(CORS());
+
+// setting the proxy configs
+app.use('/proxy');
+
+// set the routes 
+app.get('/');
+
+module.exports = {
+    app
+};
