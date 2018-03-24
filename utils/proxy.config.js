67743e3d3dbefd6ed050ddcc1b51208a3a5cb288
Ali Almohaya <alialmohaya@gmail.com>
2018-03-24 22:13:37 +0300

    init the proxy

---
+++ utils/proxy.config.js
@@ -0,0 +1,24 @@
+const proxy = require('http-proxy-middleware');
+
+/**
+ * @param {expressApp} app: the current express app
+ * @returns null
+ * 
+ * @description: sets the proxy to the express app
+ * @type with side effects
+ */
+const mainProxy = (app) => {
+    app.use('/proxy', proxy({
+        onProxyReq: onMainProxyReq
+    }));
+};
+
+
+const onMainProxyReq = (proxyReq, req, res) => {
+    const requstedUrl = req.query.url;
+    proxyReq.path - requstedUrl;
+};
+
+module.exports = {
+    mainProxy
+}
