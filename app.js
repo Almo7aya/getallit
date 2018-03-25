17c58f0dbb55ea7b847c6dbff8afde0ead0a2f67
Ali Almohaya <alialmohaya@gmail.com>
2018-03-25 23:52:43 +0300

    test facebook site

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(CORS());
 app.get('**', (req, client_res) => {
 
     var options = {
-        hostname: 'www.google.com',
+        hostname: 'www.facebook.com',
         port: 80,
         path: req.url,
         method: 'GET'
